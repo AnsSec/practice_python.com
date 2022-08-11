7aee919f5e1c2f951ba68f319e093e6946c46cd2
Anshul <72655193+AnsSec@users.noreply.github.com>
2022-08-11 13:01:36 +0530

    Create Ex_1_Character_Input.py

---
+++ Ex_1_Character_Input.py
@@ -0,0 +1,4 @@
+name=str(input("Enter Your Name :"))
+age=int(input("Enter your Age : "))
+year=2022 - age + 100
+print(f"Hello {name} Your age is {age} the year that you will turn 100 years old is {year} ")
