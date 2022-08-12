43196948f1faf4cde992d627e408912ad0d41207
Anshul <72655193+AnsSec@users.noreply.github.com>
2022-08-12 09:19:35 +0530

    Create Ex_3_List_Less_Than_Ten.py

---
+++ Ex_3_List_Less_Than_Ten.py
@@ -0,0 +1,4 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+print("1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89")
+b=int(input("Enter Your Number Which you want to print Less then in above list: "))
+print(a[0:b])
